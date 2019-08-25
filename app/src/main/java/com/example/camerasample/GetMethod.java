9e248e3ce4f189cdad200cc04b1b14c43190c8d6
music-mind <arriye_@hotmail.com>
2019-08-24 22:12:21 -0400

    api stuff

---
+++ app/src/main/java/com/example/camerasample/GetMethod.java
@@ -0,0 +1,153 @@
+package com.example.camerasample;
+
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class GetMethod extends AsyncTask<String , Void ,String> {
+    String server_response;
+    String key;
+    Bitmap bitmap;
+    String attachmentName = "bitmap";
+    String attachmentFileName = "bitmap.bmp";
+    String crlf = "\r\n";
+    String twoHyphens = "--";
+    String boundary =  "*****";
+
+    GetMethod(String key, Bitmap bmp) {
+        this.key = key;
+        this.bitmap = bmp;
+    }
+
+    @Override
+    protected String doInBackground(String... strings) {
+
+        URL url;
+        HttpURLConnection urlConnection = null;
+
+        try {
+            url = new URL(strings[0]);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setUseCaches(false);
+            urlConnection.setDoOutput(true);
+
+            urlConnection.setRequestMethod("POST");
+            urlConnection.setRequestProperty("Connection", "Keep-Alive");
+            urlConnection.setRequestProperty("Cache-Control", "no-cache");
+            //urlConnection.setRequestProperty("Content-Type", "application/octet-stream");
+            urlConnection.setRequestProperty(
+                    "Content-Type", "multipart/form-data;boundary=" + this.boundary);
+            urlConnection.setRequestProperty("Ocp-Apim-Subscription-Key", key);
+
+            DataOutputStream request = new DataOutputStream(
+                    urlConnection.getOutputStream());
+
+            request.writeBytes(this.twoHyphens + this.boundary + this.crlf);
+            request.writeBytes("Content-Disposition: form-data; name=\"" +
+                    this.attachmentName + "\";filename=\"" +
+                    this.attachmentFileName + "\"" + this.crlf);
+            request.writeBytes(this.crlf);
+
+            byte[] pixels = new byte[bitmap.getWidth() * bitmap.getHeight()];
+            for (int i = 0; i < bitmap.getWidth(); ++i) {
+                for (int j = 0; j < bitmap.getHeight(); ++j) {
+                    //we're interested only in the MSB of the first byte,
+                    //since the other 3 bytes are identical for B&W images
+                    pixels[i + j] = (byte) ((bitmap.getPixel(i, j) & 0x80) >> 7);
+                }
+            }
+
+//            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+//            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+//            byte[] byteArray = stream.toByteArray();
+//            bitmap.recycle();
+
+            request.write(pixels);
+            request.writeBytes(this.crlf);
+            request.writeBytes(this.twoHyphens + this.boundary +
+                    this.twoHyphens + this.crlf);
+            request.flush();
+            request.close();
+
+            int responseCode = urlConnection.getResponseCode();
+            System.out.println(responseCode);
+
+            if(responseCode == HttpURLConnection.HTTP_ACCEPTED){
+                InputStream responseStream = new
+                        BufferedInputStream(urlConnection.getInputStream());
+
+                BufferedReader responseStreamReader =
+                        new BufferedReader(new InputStreamReader(responseStream));
+
+                String line = "";
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ((line = responseStreamReader.readLine()) != null) {
+                    stringBuilder.append(line).append("\n");
+                }
+                responseStreamReader.close();
+
+                String response = stringBuilder.toString();
+                Log.e("Response", " " + response);
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if(urlConnection != null) {
+                urlConnection.disconnect();
+            }
+        }
+
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+
+        //Log.e("Response", "s" + server_response);
+
+
+    }
+
+
+// Converting InputStream to String
+
+    private String readStream(InputStream in) {
+        BufferedReader reader = null;
+        StringBuffer response = new StringBuffer();
+        try {
+            reader = new BufferedReader(new InputStreamReader(in));
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return response.toString();
+    }
+}
--- app/src/main/java/com/example/camerasample/MainActivity.java
+++ app/src/main/java/com/example/camerasample/MainActivity.java
@@ -4,11 +4,21 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Environment;
+import android.os.StrictMode;
 import android.provider.MediaStore;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -18,32 +28,83 @@ import java.io.PrintStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import com.google.cloud.vision.v1.AnnotateImageRequest;
-import com.google.cloud.vision.v1.AnnotateImageResponse;
-import com.google.cloud.vision.v1.BatchAnnotateImagesResponse;
-import com.google.cloud.vision.v1.EntityAnnotation;
-import com.google.cloud.vision.v1.Feature;
-import com.google.cloud.vision.v1.Feature.Type;
-import com.google.cloud.vision.v1.Image;
-import com.google.cloud.vision.v1.ImageAnnotatorClient;
-import com.google.protobuf.ByteString;
+//import com.microsoft.azure.cognitiveservices.vision.computervision.*;
+//import com.microsoft.azure.cognitiveservices.vision.computervision.models.*;
+
+import java.io.File;
+import java.io.FileInputStream;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+
 import java.util.ArrayList;
 import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
 
     String currentPhotoPath;
     static final int REQUEST_TAKE_PHOTO = 1;
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    String azureComputerVisionApiKey;
+    String endpointUrl;
+    //ComputerVisionClient compVisClient;
+    Bitmap bitmap;
+    public static String baseUrl = "https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/analyze?visualFeatures=Categories,Description,Brands";
+    public static HttpClient client = new DefaultHttpClient();
+    public static HttpPost httpPost = new HttpPost(baseUrl);
+    public static ArrayList<NameValuePair> myParams = new ArrayList<>(1);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        StrictMode.ThreadPolicy policy = new
+                StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        //azureComputerVisionApiKey = System.getenv("COMPUTER_VISION_SUBSCRIPTION_KEY");
+        azureComputerVisionApiKey = "a4d18db961af49748102472e13105095";
+        System.out.println("KEY: " + azureComputerVisionApiKey);
+        endpointUrl = "https://westcentralus.api.cognitive.microsoft.com/vision/v2.0";
+//        compVisClient = ComputerVisionManager.authenticate(azureComputerVisionApiKey).withEndpoint(endpointUrl);
+        //  END - Create an authenticated Computer Vision client.
     }
+
+//    public static void AnalyzeLocalImage(ComputerVisionClient compVisClient, Bitmap bmp) {
+//        /*  Analyze a local image:
+//         *
+//         *  Set a string variable equal to the path of a local image. The image path below is a relative path.
+//         */
+//        //String pathToLocalImage = "src\\main\\resources\\starbucks.jpg";
+//        //  This list defines the features to be extracted from the image.
+//        List<VisualFeatureTypes> featuresToExtractFromLocalImage = new ArrayList<>();
+//        featuresToExtractFromLocalImage.add(VisualFeatureTypes.DESCRIPTION);
+//        featuresToExtractFromLocalImage.add(VisualFeatureTypes.TAGS);
+//
+//        //  Need a byte array for analyzing a local image.
+//        //File rawImage = new File(pathToLocalImage);
+//        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+//        bmp.compress(Bitmap.CompressFormat.PNG, 100, stream);
+//        byte[] imageByteArray = stream.toByteArray();
+//        bmp.recycle();
+//        try {
+//            //byte[] imageByteArray = Files.readAllBytes(rawImage.toPath());
+//            //  Call the Computer Vision service and tell it to analyze the loaded image.
+//            ImageAnalysis analysis = compVisClient.computerVision().analyzeImageInStream()
+//                    .withImage(imageByteArray)
+//                    .withVisualFeatures(featuresToExtractFromLocalImage)
+//                    .execute();
+//
+//            //  Display image captions and confidence values.
+//            System.out.println("\nCaptions: ");
+//            for (ImageCaption caption : analysis.description().captions()) {
+//                System.out.printf("\'%s\' with confidence %f\n", caption.text(), caption.confidence());
+//            }
+//        } catch (Exception e) {
+//            System.out.println(e.getMessage());
+//            e.printStackTrace();
+//        }
+//
+//    }
 
     private File createImageFile() throws IOException {
         // Create an image file name
@@ -99,47 +160,73 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
-            try {
-                this.detectLogos(imageBitmap, System.out);
-            } catch (IOException e) {
-
-            } catch (Exception ex) {}
+            this.bitmap = imageBitmap;
+            //new GetMethod(azureComputerVisionApiKey, imageBitmap).execute(baseUrl);
+            //AnalyzeLocalImage(compVisClient, imageBitmap);
+            getImage();
+//            try {
+//                this.detectLogos(imageBitmap, System.out);
+//            } catch (IOException e) {
+//
+//            } catch (Exception ex) {}
             //imageView.setImageBitmap(imageBitmap);
         }
     }
 
-    public static void detectLogos(Bitmap bmp, PrintStream out) throws Exception, IOException {
-        List<AnnotateImageRequest> requests = new ArrayList<>();
+    public void getImage(){
+        httpPost.setHeader("Ocp-Apim-Subscription-Key", azureComputerVisionApiKey);
+        httpPost.setHeader("Content-Type", "application/octet-stream");
 
-        ByteArrayOutputStream stream = new ByteArrayOutputStream();
-        bmp.compress(Bitmap.CompressFormat.PNG, 100, stream);
-        byte[] byteArray = stream.toByteArray();
-        bmp.recycle();
+        try{
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            this.bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            byte[] imageByteArray = stream.toByteArray();
+            this.bitmap.recycle();
 
+            httpPost.setEntity(new UrlEncodedFormEntity(myParams, "UTF-8"));
+            httpPost.setEntity(new ByteArrayEntity(imageByteArray));
+            HttpResponse response =  client.execute(httpPost);
 
-        ByteString imgBytes = ByteString.copyFrom(byteArray);
-
-        Image img = Image.newBuilder().setContent(imgBytes).build();
-        Feature feat = Feature.newBuilder().setType(Type.LOGO_DETECTION).build();
-        AnnotateImageRequest request =
-                AnnotateImageRequest.newBuilder().addFeatures(feat).setImage(img).build();
-        requests.add(request);
-
-        try (ImageAnnotatorClient client = ImageAnnotatorClient.create()) {
-            BatchAnnotateImagesResponse response = client.batchAnnotateImages(requests);
-            List<AnnotateImageResponse> responses = response.getResponsesList();
-
-            for (AnnotateImageResponse res : responses) {
-                if (res.hasError()) {
-                    out.printf("Error: %s\n", res.getError().getMessage());
-                    return;
-                }
-
-                // For full list of available annotations, see http://g.co/cloud/vision/docs
-                for (EntityAnnotation annotation : res.getLogoAnnotationsList()) {
-                    out.println(annotation.getDescription());
-                }
-            }
+            System.out.println(EntityUtils.toString(response.getEntity()));
+        }catch(Exception e){
+            System.out.println("ERRRRR"  + e.toString());
         }
     }
+
+
+
+//    public static void detectLogos(Bitmap bmp, PrintStream out) throws Exception, IOException {
+//        List<AnnotateImageRequest> requests = new ArrayList<>();
+//
+//        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+//        bmp.compress(Bitmap.CompressFormat.PNG, 100, stream);
+//        byte[] byteArray = stream.toByteArray();
+//        bmp.recycle();
+//
+//
+//        ByteString imgBytes = ByteString.copyFrom(byteArray);
+//
+//        Image img = Image.newBuilder().setContent(imgBytes).build();
+//        Feature feat = Feature.newBuilder().setType(Type.LOGO_DETECTION).build();
+//        AnnotateImageRequest request =
+//                AnnotateImageRequest.newBuilder().addFeatures(feat).setImage(img).build();
+//        requests.add(request);
+//
+//        try (ImageAnnotatorClient client = ImageAnnotatorClient.create()) {
+//            BatchAnnotateImagesResponse response = client.batchAnnotateImages(requests);
+//            List<AnnotateImageResponse> responses = response.getResponsesList();
+//
+//            for (AnnotateImageResponse res : responses) {
+//                if (res.hasError()) {
+//                    out.printf("Error: %s\n", res.getError().getMessage());
+//                    return;
+//                }
+//
+//                // For full list of available annotations, see http://g.co/cloud/vision/docs
+//                for (EntityAnnotation annotation : res.getLogoAnnotationsList()) {
+//                    out.println(annotation.getDescription());
+//                }
+//            }
+//        }
+//    }
 }
